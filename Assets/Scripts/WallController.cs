ac222a18b677b364818030d7892bcff5e9c8ef4b
HawoonShin <sinhaoon@gmail.com>
2024-09-26 15:18:59 +0900

    [FEAT] 방벽 구현
    방벽의 체력이 다 닳을 경우 파괴됨

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -9,6 +9,14 @@ public class WallController : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log($"{wallHp}");
+        // Debug.Log($"{wallHp}");
+
+        // 방벽의 체력이 0이 될 경우
+        if (wallHp < 0)
+        {
+            // 방벽 파괴
+            Destroy(gameObject);    
+            // 게임 오버
+        }
     }
 }
