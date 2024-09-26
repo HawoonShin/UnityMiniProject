4609555de57fc3ce44c176393d26b4b9937e8f75
HawoonShin <sinhaoon@gmail.com>
2024-09-26 17:37:19 +0900

    [FEAT] 총 기능 추가
    누락하였던 재장전 기능 추가

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,47 +4,56 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // TODO
-    // 전체 탄창수 
-    // R키로 
-    // 재장전
-    // 탄창을 다 사용했을 경우
-    // 자동 재장전
-
-    // 총알이 몬스터와 충돌하였을 경우
-    // 몬스터의 체력을 감소
-
     // 총알 프리팹
     [SerializeField] GameObject bulletPrefab;
     // 탄속
     [SerializeField] float bulletSpeed;
+    // (임시) 탄창수
+    [SerializeField] float bulletNumber;
+    private float curBulletNumber;
 
+    private void Start()
+    {
+        curBulletNumber = bulletNumber;
+    }
 
     private void Update()
     {
         // 좌클릭 입력
+        // 누르고 있는 동안 발사할 경우 Down만 제거하면 되나?
         if (Input.GetMouseButtonDown(0))
         {
             // 발사
             Shoot();
         }
+        // 자동 재장전
+        if(curBulletNumber<0)
+        {
+            curBulletNumber = bulletNumber;
+            Debug.Log($"자동 재장전! (현재 탄수 : {curBulletNumber})");
+        }
+        // R버튼으로 재장전
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            curBulletNumber = bulletNumber;
+            Debug.Log($"재장전! (현재 탄수 : {curBulletNumber})");
+        }
     }
 
     private void Shoot()
     {
-        // 발사될 방향 필요
-        // Vector2.forward = new Vector2 (회전후 방향으로부터 우측으로 직선상의 부분)
-        // 같은 느낌으로 만들면 될것도 같고..
-        // 누르고 있는 동안 계속 발사
+
         // 발사 사이에 딜레이 필요?
 
         // 총알 생성
         GameObject bullet = Instantiate(bulletPrefab);
        // 총 위치에 총알 생성
         bullet.transform.position = gameObject.transform.position;
-        // 총알 이동
-       
-        
+
+        Debug.Log($"발사! (현재탄수 : {curBulletNumber})");
+
+        // (임시) 탄창 수 감소
+        curBulletNumber -= 1;
     }
 
 }
